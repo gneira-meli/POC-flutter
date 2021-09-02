5891157828d148d85c1c8a9804e5d6d474d51f0c
Gabriel Neira <gabriel.neira@mercadolibre.com.co>
2021-09-01 20:23:50 -0500

    Create initial ios test project

---
+++ ios_test_app/TestApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Gabriel Fernando Neira Bermudez on 30/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func didClickButton(_ sender: Any) {
+        
+        
+    }
+    
+}
+
